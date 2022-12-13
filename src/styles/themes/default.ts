e6414af2a4a21fd48e37733cc39bed24ccc455a0
Danilo dos Santos <danilo.santossouza@outlook.com>
2022-12-13 19:55:40 -0300

    Configuração do projeto do zero, com ESLint e StyledComponent

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  while: '#FFFFFF',
+
+  'background': '#FAFAFA',
+
+  'yellow-700': '#C47F17',
+  'yellow-400': '#DBAC2C',
+  'yellow-100': '#F1E9C9',
+
+  'purple-700': '#4B2995',
+  'purple-400': '#8047F8',
+  'purple-100': '#EBE5F9',
+
+  'gray-100': '#F3F2F2',
+  'gray-300': '#EDEDED',
+  'gray-400': '#E6E5E5',
+  'gray-500': '#D7D5D5',
+  'gray-600': '#8D8686',
+  'gray-700': '#574F4D',
+  'gray-800': '#403937',
+  'gray-900': '#272221',
+}
